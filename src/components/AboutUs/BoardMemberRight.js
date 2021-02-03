be660c6c08e1207b14cf6f9870e4f1ced867f105
Milton Chung <miltonjchung@gmail.com>
2021-02-02 22:49:20 -0800

    board members

--- src/components/AboutUs/BoardMemberRight.js
+++ src/components/AboutUs/BoardMemberRight.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-const BoardMemberRight = () => {
-	return <div>asd</div>;
+const BoardMemberRight = ({ name, position, profile, message }) => {
+	return (
+		<div className="board-member-right">
+			<div className="chat-bubble">
+				<p className="chat-bubble-name">
+					{name}, {position}
+				</p>
+				<p className="chat-bubble-text chat-bubble-bottom-right">{message}</p>
+			</div>
+			<img className="avatar" src={profile} alt={name} />
+		</div>
+	);
 };
 
 export default BoardMemberRight;
