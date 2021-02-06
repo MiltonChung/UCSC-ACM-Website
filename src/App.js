85fa803222190c7eed2825ca22124cfd24dc4ff5
Milton Chung <miltonjchung@gmail.com>
2021-02-06 00:56:05 -0800

    bottom navbar test

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 // Styles
 import "./styles/main.scss";
 // NPM Packages
@@ -13,19 +14,54 @@ import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 
 function App() {
+	const [windowDimension, setWindowDimension] = useState(null);
+
+	useEffect(() => {
+		setWindowDimension(window.innerWidth);
+	}, []);
+
+	useEffect(() => {
+		function handleResize() {
+			setWindowDimension(window.innerWidth);
+		}
+
+		window.addEventListener("resize", handleResize);
+		return () => window.removeEventListener("resize", handleResize);
+	}, []);
+
+	const isMobile = windowDimension <= 500;
+
 	return (
 		<div className="App">
 			<BrowserRouter>
-				<Nav />
-				<Switch>
-					<Route exact path="/" component={Home} />
-					<Route path="/aboutus" component={AboutUs} />
-					<Route path="/events" component={Events} />
-					<Route path="/resources" component={Resources} />
-					<Route path="/contact" component={Contact} />
-					<Redirect to="/" />
-				</Switch>
-				<Footer />
+				{isMobile ? (
+					<>
+						<Switch>
+							<Route exact path="/" component={Home} />
+							<Route path="/aboutus" component={AboutUs} />
+							<Route path="/events" component={Events} />
+							<Route path="/resources" component={Resources} />
+							<Route path="/contact" component={Contact} />
+							<Redirect to="/" />
+						</Switch>
+						<Footer />
+						<Nav />
+					</>
+				) : (
+					<>
+						<Nav />
+						<Switch>
+							<Route exact path="/" component={Home} />
+							<Route path="/aboutus" component={AboutUs} />
+							<Route path="/events" component={Events} />
+							<Route path="/resources" component={Resources} />
+							<Route path="/contact" component={Contact} />
+							<Redirect to="/" />
+						</Switch>
+
+						<Footer />
+					</>
+				)}
 			</BrowserRouter>
 		</div>
 	);
