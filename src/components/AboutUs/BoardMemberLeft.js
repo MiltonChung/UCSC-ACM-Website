3857cb1190956490d3670e84c92a225dae9165a1
Milton Chung <miltonjchung@gmail.com>
2021-02-06 22:24:10 -0800

    board members responsive

--- src/components/AboutUs/BoardMemberLeft.js
+++ src/components/AboutUs/BoardMemberLeft.js
@@ -6,7 +6,7 @@ const BoardMemberLeft = ({ name, position, profile, message, id }) => {
 		<div className="board-member-left" key={id}>
 			<img className="avatar" src={profile} alt={name} />
 			<div className="chat-bubble">
-				<p className="chat-bubble-names">
+				<p className="chat-bubble-name">
 					{name}, {position}
 				</p>
 				<div className="chat-bubble-text chat-bubble-bottom-left">
