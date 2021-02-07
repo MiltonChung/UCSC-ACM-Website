50321eac49eff58e2a7c3f5abe72d566612a1372
Milton Chung <miltonjchung@gmail.com>
2021-02-06 22:41:12 -0800

    nav + scrollTop component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 // Styles
 import "./styles/main.scss";
 // NPM Packages
@@ -12,56 +11,23 @@ import Resources from "./pages/Resources";
 // Components
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
+import ScrollTop from "./components/ScrollTop";
 
 function App() {
-	const [windowDimension, setWindowDimension] = useState(null);
-
-	useEffect(() => {
-		setWindowDimension(window.innerWidth);
-	}, []);
-
-	useEffect(() => {
-		function handleResize() {
-			setWindowDimension(window.innerWidth);
-		}
-
-		window.addEventListener("resize", handleResize);
-		return () => window.removeEventListener("resize", handleResize);
-	}, []);
-
-	const isMobile = windowDimension <= 500;
-
 	return (
 		<div className="App">
 			<BrowserRouter>
-				{isMobile ? (
-					<>
-						<Switch>
-							<Route exact path="/" component={Home} />
-							<Route path="/aboutus" component={AboutUs} />
-							<Route path="/events" component={Events} />
-							<Route path="/resources" component={Resources} />
-							<Route path="/contact" component={Contact} />
-							<Redirect to="/" />
-						</Switch>
-						<Footer />
-						<Nav />
-					</>
-				) : (
-					<>
-						<Nav />
-						<Switch>
-							<Route exact path="/" component={Home} />
-							<Route path="/aboutus" component={AboutUs} />
-							<Route path="/events" component={Events} />
-							<Route path="/resources" component={Resources} />
-							<Route path="/contact" component={Contact} />
-							<Redirect to="/" />
-						</Switch>
-
-						<Footer />
-					</>
-				)}
+				<Nav />
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route path="/aboutus" component={AboutUs} />
+					<Route path="/events" component={Events} />
+					<Route path="/resources" component={Resources} />
+					<Route path="/contact" component={Contact} />
+					<Redirect to="/" />
+				</Switch>
+				<Footer />
+				<ScrollTop />
 			</BrowserRouter>
 		</div>
 	);
